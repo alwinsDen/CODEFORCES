1cb1e9e4757e7dc66ccbb33f7d11f55838e4be02
alwinsDen <jsmadesimple@gmail.com>
2022-07-05 05:05:21 +0530

    CF1699A

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,13 @@
-// never solved
-use std::io::{BufRead};
-use std::collections::VecDeque;
+use std::io::BufRead;
 fn main() {
-    let ins = std::io::stdin().lock().lines().next().unwrap().unwrap().parse::<i32>().unwrap();
-    let mut si = String::new();
-    std::io::stdin().read_line(&mut si);
-    let mut sil = si.trim().split(" ").flat_map(str::parse::<i32>).collect::<VecDeque<_>>();
-    let mut dollar = 0;
-    sil.push_front(0);
-    for index in 0..sil.len() {
-        if index!=sil.len()-1 && sil[index] - sil[index + 1] < 0  {
-            if index as i32 != sil.len() as i32 -2 {
-                dollar += (sil[index] - sil[index + 1]).abs();
-                println!("dollar {}",dollar);
-            } else {
-                dollar += (sil[index]-sil[index+1]).abs();
-                println!("dollars {}",dollar);
-            }
+    let mut si = std::io::stdin().lock().lines().next().unwrap().unwrap().parse::<i32>().unwrap();
+    for _ in 0..si {
+        let mut li = std::io::stdin().lock().lines().next().unwrap().unwrap().parse::<i32>()
+            .unwrap();
+        if li%2==0 {
+            println!("{} {} {}",li/2,0,0);
+        } else {
+            println!("-1");
         }
     }
-    println!("{}",dollar);
 }
