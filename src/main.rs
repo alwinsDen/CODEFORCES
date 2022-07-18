d8b6dca3e865a15c0744ac0a615a6ec818901d25
alwinsDen <jsmadesimple@gmail.com>
2022-07-18 10:46:49 +0530

    CF1108B

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,45 @@
 use std::io::BufRead;
-use std::collections::HashSet;
-fn main() {
-    pub fn binary_search(arr: &Vec<i32>, x: i32) -> isize {
-        let mut high = arr.len() as i32 - 1;
-        let mut low = 0;
-        while high >= low {
-            let mut mid = low + (high - low) / 2;
-            if arr[mid as usize] == x {
-                return mid as isize;
-            }
-            if arr[mid as usize] > x {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+use std::process::exit;
+
+fn binary_search(arr: &Vec<i32>, x: i32) -> i32 {
+    let mut high = (arr.len() - 1) as i32;
+    let mut low = 0;
+    while high >= low {
+        let mut mid = low + (high - low) / 2;
+        if arr[mid as usize] == x {
+            return mid;
         }
-        return -1;
-    }
-    let mut k = std::io::stdin().lock().lines().next().unwrap().unwrap().parse::<i32>().unwrap();
-    for _ in 0..k {
-        let mut l1 = String::new();
-        let mut l2 = String::new();
-        std::io::stdin().read_line(&mut l1);
-        std::io::stdin().read_line(&mut l2);
-        let mut l1 = l1.trim().split(" ").flat_map(str::parse::<i32>).collect::<Vec<_>>();
-        let mut l2 = l2.trim().split(" ").flat_map(str::parse::<i32>).collect::<Vec<_>>();
-        let mut l3 = Vec::new();
-        'csm: for i in 0..l1[0] {
-            if i == l1[0 as usize] - 1 {
-                break 'csm;
-            }
-            if l2[i as usize] < l2[i as usize + 1] {
-                l3.extend([l2[i as usize], l2[i as usize + 1]]);
-            }
-        }
-        l2.sort();
-        let mut sorted_count = 0;
-        let mut lst = 1000001;
-        let mut hsl = HashSet::new();
-        'rsm: for (n, k) in l3.iter().enumerate() {
-            if n % 2 == 1 { continue; }
-            let mut rt = binary_search(&l2, *k);
-            if rt == -1 { continue; }
-            if &l2[rt as usize] == k && l2[rt as usize + 1] == l3[n + 1] {
-                hsl.insert(l2[rt as usize]);
-                hsl.insert(l2[rt as usize + 1]);
-                if lst==l2[rt as usize] {
-                    lst=l2[rt as usize + 1];
-                    continue 'rsm;
-                }
-                sorted_count += 1;
-                lst=l2[rt as usize + 1];
-            }
-        }
-        let srp = l1[0] - hsl.len() as i32;
-        let nsrp = l1[1] - sorted_count;
-        if srp <= nsrp {
-           println!("YES");
+        if arr[mid as usize] > x {
+            high = mid - 1;
         } else {
-            println!("NO");
+            low = mid + 1;
         }
     }
+    -1
+}
+
+fn main() {
+    let mut m = std::io::stdin().lock().lines().next().unwrap().unwrap().parse::<i32>().unwrap();
+    let mut l1 = String::new();
+    std::io::stdin().read_line(&mut l1);
+    let mut l1 = l1.trim().split(" ").flat_map(str::parse::<i32>).collect::<Vec<_>>();
+    l1.sort();
+    if m == 2 {
+        println!("1 1");
+        exit(0);
+    }
+    let mut cl1 = l1.clone();
+    let mut val_list: Vec<i32> = Vec::new();
+    for l in 0..l1[l1.len() - 1] + 1 {
+        if l != 0 && l1[l1.len() - 1] % l == 0 {
+            val_list.push(binary_search(&cl1, l));
+        }
+    }
+    // println!("{}",cl1.len());
+    let mut offset = 0;
+    for (m, j) in val_list.iter().enumerate() {
+        cl1.remove(*j as usize - offset as usize);
+        offset += 1;
+    }
+    println!("{:?} {:?}", l1[l1.len() - 1], cl1[cl1.len()-1]);
 }
